15cf1c3de127cb08c3222c3c067958b9b8eb1bfc
Aniket999969 <72073185+Aniket999969@users.noreply.github.com>
2020-12-01 22:40:56 +0530

    1.0.02
    ADDING OWN ADMIN SYSTEM WITH SQL

--- client.lua
+++
@@ -1,3 +0,0 @@
-AddEventHandler('playerSpawned', function()
-TriggerServerEvent('HellFW:SpawnPlanter')
-end)
--- server.lua
+++
@@ -1,4 +0,0 @@
-RegisterServerEvent ('HellFW:SpawnPlayer')
-AddEventHandler('HellFW:SpawnPlayer' , function()
-    local source = source
-    exports.ghmattisql:execute('SELECT * FROM users WHERE identifer + @identifer')
