154668f79003cc9ba21124fc06093c453ab9c8ed
Aniket999969 <72073185+Aniket999969@users.noreply.github.com>
2021-01-05 20:55:24 +0530

    Hell-FrameWork 1.2
    Optimized The Server

--- [Assest]/Hell-Extras/fxmanifest.lua
+++ [Assest]/Hell-Extras/fxmanifest.lua
@@ -4,6 +4,10 @@ games { 'gta5' }
 ui_page 'html_car/index.html'
 ui_page 'bank_html/ui.html'
 ui_page 'bwh_html/index.html'
+ui_page 'hack.html'
+ui_page "html_radar/radar.html"
+ui_page 'html/ui.html'
+
 client_script {
     
     '3dme_c.lua',
@@ -15,6 +19,27 @@ client_script {
     'bwh_c.lua',
     'bwh_config.lua',
     'heli_client.lua',
+    'isped_c.lua',
+    'vehcontrol_c.lua',
+    'mhacking.lua',
+    'sequentialhack.lua',
+    'chairs.lua',
+    'chair_c.lua',
+    'chair_config.lua',
+    'NativeUI.lua',
+    'wpdeleter_c.lua',
+    'cl_radar.lua',
+    'ss_shared_functions.lua',
+    'config/Keybinds.lua',
+    'config/ServerSync.lua',
+    'ss_cli_traffic_crowd.lua',
+    'ss_cli_weather.lua',
+    'ss_cli_time.lua',
+    'shop_c.lua',
+    'shop_gui.lua',
+    'race_config.lua',
+    'races_cl.lua',
+    'radio_cl.lua',
 }  
 
 files {
@@ -50,16 +75,99 @@ files {
     'bwh_html/jquery.datetimepicker.min.css',
     'bwh_html/jquery.datetimepicker.full.min.js',
     'bwh_html/date.format.js'
+    'phone.png',
+    'snd/beep.ogg',
+    'snd/correct.ogg',
+    'snd/fail.ogg', 
+    'snd/start.ogg',
+    'snd/finish.ogg',
+    'snd/wrong.ogg',
+    'hack.html'
+    "html_radar/digital-7.regular.ttf", 
+	"html_radar/radar.html",
+	"html_radar/radar.css",
+    "html_radar/radar.js"
+    'html/icons/properties.png',
+	'html/icons/properties2.png',
+	'html/icons/decrypt.png',
+	'html/icons/groups.png',
+	'html/icons/blackmarkettasks.png',
+	'html/icons/delivery.png',
+	'html/icons/stocks.png',
+	'html/icons/camera.png',
+	'html/icons/help.png',
+	'html/icons/csms.png',
+	'html/icons/home.png',
+	'html/icons/call.png',
+	'html/icons/call2.png',
+	'html/icons/chathistory.png',
+	'html/icons/trash.png',
+	'html/icons/account.png',
+	'html/icons/car.png',
+	'html/icons/jobalerts.png',
+	'html/icons/twatter.png',
+	'html/icons/contacts.png',
+	'html/icons/gps.png',
+	'html/icons/internet.png',
+	'html/icons/newsms.png',
+	'html/icons/notepad.png',
+	'html/icons/pager.png',
+	'html/icons/pager2.png',
+	'html/icons/pager3.png',
+	'html/icons/phone.png',
+	'html/icons/phonenumber.png',
+	'html/icons/sms.png',
+	'html/icons/yellowpages.png',
+	'html/icons/packages.png',
+	'html/icons/trucker.png',
+	'html/ui.html',
+	'html/gurgle.png',
+	'html/pricedown.ttf',
+	'html/cursor.png',
+	'html/background.png',
+	'html/backgroundwhite.png',
+	'html/styles.css',
+	'html/scripts.js',
+	'html/debounce.min.js'
 }
 server_script {
- '@mysql-async/lib/MySQL.lua',
- 'server.lua',
- 'server/sv_queue_config.lua',
- 'connectqueue.lua',
- 'sh_queue.lua',
- 'cron_s.lua',
- 'bwh_config.lua'
- 'bwh_s.lua',
- 'heli_server.lua',
+    '@mysql-async/lib/MySQL.lua',
+    'server.lua',
+    'server/sv_queue_config.lua',
+    'connectqueue.lua',
+    'sh_queue.lua',
+    'cron_s.lua',
+    'bwh_config.lua'
+    'bwh_s.lua',
+    'heli_server.lua',
+    'isped_s.lua',
+    'vehcontrol_s.lua',
+    'chair_config.lua'
+    'chairs.lua',
+    'chair_s.lua',
+    'wpdeleter_s.lua',
+    'ss_shared_functions.lua',
+    'config/ServerSync.lua',
+    'ss_srv_weather.lua',
+    'ss_srv_time.lua'
+    "race_config.lua",
+    "port_sv.lua",
+    "races_sv.lua",
 }
+
 shared_script 'discord_config.lua'
+
+
+export "GetClosestNPC"
+export "IsPedNearCoords"
+export "isPed"
+export "GroupRank"
+export "GlobalObject"
+export "retreiveBusinesses"
+
+
+exports {
+    'cleanupRecording',
+    'isRecordingRace',
+    'cpCount'
+}
